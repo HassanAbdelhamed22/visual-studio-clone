646b10516df0fa5c77210f10c10c8d328b05802e
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-16 20:30:00 +0200

    create file component and file svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import FileComponents from "./components/FileComponents";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </>
+    <div className="m-7">
+      <FileComponents fileName="index.tsx" />
+      <FileComponents fileName="index.html" />
+    </div>
   );
 }
 
