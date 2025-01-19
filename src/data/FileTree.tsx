f9f1b5622f02edbd7ab93c3b805fa7d79605f40a
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-19 06:30:31 +0200

    add files and folders in file tree obj

--- src/data/FileTree.tsx
+++ src/data/FileTree.tsx
@@ -7,6 +7,26 @@ export const fileTree: IFile = {
     {
       name: "node_modules",
       isFolder: true,
+      children: [
+        {
+          name: ".vite",
+          isFolder: true,
+          children: [
+            {
+              name: "tsconfig.app.json",
+              isFolder: false,
+            },
+            {
+              name: "tsconfig.node.json",
+              isFolder: false,
+            },
+          ],
+        },
+        {
+          name: "react-dom",
+          isFolder: false,
+        },
+      ],
     },
     {
       name: "index.tsx",
