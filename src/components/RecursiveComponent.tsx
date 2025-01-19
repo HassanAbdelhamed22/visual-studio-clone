e1f5d200d4cf065895905ebc923d8b661eac28be
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-19 07:20:53 +0200

    working on file icons

--- src/components/RecursiveComponent.tsx
+++ src/components/RecursiveComponent.tsx
@@ -1,23 +1,41 @@
 import FileIcon from "./SVG/File";
 import RightArrowIcon from "./SVG/Right";
 import { IFile } from "../interfaces";
+import FolderIcon from "./SVG/Folder";
+import { useState } from "react";
+import BottomArrowIcon from "./SVG/Bottom";
 
 interface IProps {
   fileTree: IFile;
 }
 
-const RecursiveComponent = ({ fileTree }: IProps) => {
+const RecursiveComponent = ({
+  fileTree: { name, isFolder, children, content },
+}: IProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // ** Handlers
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div className="mb-2 ml-2">
+    <div className="mb-2 ml-2 cursor-pointer">
       <div className="flex items-center mb-1">
-        <RightArrowIcon />
-        <span className="mr-2">
-          <FileIcon />
-        </span>
-        <span>{fileTree.name}</span>
+        {isFolder ? (
+          <div onClick={toggle} className="flex items-center">
+            {isOpen ? <RightArrowIcon /> : <BottomArrowIcon />}
+            <FolderIcon />
+          </div>
+        ) : (
+          <span className="mr-2">
+            <FileIcon />
+          </span>
+        )}
+        <span>{name}</span>
       </div>
 
-      {fileTree.children?.map((file, index) => (
+      {children?.map((file, index) => (
         <RecursiveComponent fileTree={file} key={index} />
       ))}
     </div>
