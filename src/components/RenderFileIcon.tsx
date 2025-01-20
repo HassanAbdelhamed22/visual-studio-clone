10fc2070bc7fbd692aac2c01553491f4b587c689
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-21 00:47:00 +0200

    code refactor

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,3 +1,4 @@
+import { extensionIconPaths } from "../constant";
 import IconImg from "./IconImg";
 import FileIcon from "./SVG/File";
 
@@ -10,60 +11,18 @@ interface IProps {
 const RenderFileIcon = ({ fileName, isFolder, isOpen }: IProps) => {
   const extension = fileName.split(".").pop();
 
-  // ** File icons
-  if (extension === "tsx") return <IconImg src="/icons/react_ts.svg" />;
-  if (extension === "js") return <IconImg src="/icons/javascript.svg" />;
-  if (extension === "json") return <IconImg src="/icons/json.svg" />;
-  if (extension === "jsx") return <IconImg src="/icons/react.svg" />;
-  if (extension === "svg") return <IconImg src="/icons/svg.svg" />;
-  if (extension === "html") return <IconImg src="/icons/html.svg" />;
-  if (extension === "ts") return <IconImg src="/icons/typescript.svg" />;
-  if (extension === "gitignore") return <IconImg src="/icons/git.svg" />;
-  if (extension === "md") return <IconImg src="/icons/readme.svg" />;
+  if (
+    extension &&
+    Object.prototype.hasOwnProperty.call(extensionIconPaths, extension)
+  ) {
+    const iconPath = isFolder
+      ? isOpen
+        ? `${extensionIconPaths[extension]}-open.svg`
+        : `${extensionIconPaths[extension]}.svg`
+      : `${extensionIconPaths[extension]}.svg`;
 
-  // ** Folder icon
-  if (extension === "node_modules")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-node-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-node.svg" />
-    );
-  if (extension === "public")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-public-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-public.svg" />
-    );
-  if (extension === "src")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-src-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-src.svg" />
-    );
-  if (extension === "components")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-components-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-components.svg" />
-    );
-  if (extension === "styles")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-stylus-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-stylus.svg" />
-    );
-  if (extension === "utils")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-utils-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-utils.svg" />
-    );
-  if (extension === "assets")
-    return isFolder && isOpen ? (
-      <IconImg src="/icons/folder-assets-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-assets.svg" />
-    );
+    return <IconImg src={iconPath} />;
+  }
 
   if (isFolder && !isOpen) return <IconImg src="/icons/folder-default.svg" />;
   if (isFolder && isOpen)
--- src/data/FileTree.tsx
+++ src/data/FileTree.tsx
@@ -61,71 +61,260 @@ export const fileTree: IFile = {
       ],
     },
     {
-      name: "src",
-      isFolder: true,
-      children: [
+      "name": "src",
+      "isFolder": true,
+      "children": [
         {
-          name: "components",
-          isFolder: true,
-          children: [
-            {
-              name: "Header.tsx",
-              isFolder: false,
-            },
-            {
-              name: "Footer.tsx",
-              isFolder: false,
-            },
-          ],
+          "name": "assets",
+          "isFolder": true,
+          "children": []
         },
         {
-          name: "styles",
-          isFolder: true,
-          children: [
+          "name": "components",
+          "isFolder": true,
+          "children": [
             {
-              name: "App.css",
-              isFolder: false,
+              "name": "Button.jsx",
+              "isFolder": false,
             },
             {
-              name: "index.css",
-              isFolder: false,
+              "name": "Header.jsx",
+              "isFolder": false,
             },
-          ],
+            {
+              "name": "Sidebar.jsx",
+              "isFolder": false,
+            },
+            {
+              "name": "Footer.jsx",
+              "isFolder": false,
+            }
+          ]
         },
         {
-          name: "assets",
-          isFolder: true,
-          children: [
-            {
-              name: "logo.svg",
-              isFolder: false,
-            },
-          ],
+          "name": "context",
+          "isFolder": true,
+          "children": []
         },
         {
-          name: "utils",
-          isFolder: true,
-          children: [
-            {
-              name: "api.ts",
-              isFolder: false,
-            },
-            {
-              name: "helpers.ts",
-              isFolder: false,
-            },
-          ],
+          "name": "hooks",
+          "isFolder": true,
+          "children": []
         },
         {
-          name: "App.tsx",
-          isFolder: false,
+          "name": "layouts",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "AdminLayout.jsx",
+              "isFolder": false
+            },
+            {
+              "name": "InstructorLayout.jsx",
+              "isFolder": false
+            },
+            {
+              "name": "StudentLayout.jsx",
+              "isFolder": false
+            }
+          ]
         },
         {
-          name: "index.tsx",
-          isFolder: false,
+          "name": "pages",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "admin",
+              "isFolder": true,
+              "children": [
+                {
+                  "name": "Dashboard.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Users.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Reports.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Settings.jsx",
+                  "isFolder": false
+                }
+              ]
+            },
+            {
+              "name": "instructor",
+              "isFolder": true,
+              "children": [
+                {
+                  "name": "Dashboard.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Courses.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Students.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Profile.jsx",
+                  "isFolder": false
+                }
+              ]
+            },
+            {
+              "name": "student",
+              "isFolder": true,
+              "children": [
+                {
+                  "name": "Dashboard.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Courses.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Quizzes.jsx",
+                  "isFolder": false
+                },
+                {
+                  "name": "Notifications.jsx",
+                  "isFolder": false
+                }
+              ]
+            }
+          ]
         },
-      ],
-    },
+        {
+          "name": "redux",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "slices",
+              "isFolder": true,
+              "children": [
+                {
+                  "name": "authSlice.js",
+                  "isFolder": false
+                },
+                {
+                  "name": "notificationSlice.js",
+                  "isFolder": false
+                },
+                {
+                  "name": "quizSlice.js",
+                  "isFolder": false
+                }
+              ]
+            },
+            {
+              "name": "store.js",
+              "isFolder": false
+            }
+          ]
+        },
+        {
+          "name": "routes",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "AdminRoutes.jsx",
+              "isFolder": false
+            },
+            {
+              "name": "InstructorRoutes.jsx",
+              "isFolder": false
+            },
+            {
+              "name": "StudentRoutes.jsx",
+              "isFolder": false
+            },
+            {
+              "name": "ProtectedRoute.jsx",
+              "isFolder": false
+            }
+          ]
+        },
+        {
+          "name": "services",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "authService.js",
+              "isFolder": false
+            },
+            {
+              "name": "quizService.js",
+              "isFolder": false
+            },
+            {
+              "name": "userService.js",
+              "isFolder": false
+            }
+          ]
+        },
+        {
+          "name": "styles",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "variables.css",
+              "isFolder": false
+            },
+            {
+              "name": "themes",
+              "isFolder": true,
+              "children": [
+                {
+                  "name": "light.css",
+                  "isFolder": false
+                },
+                {
+                  "name": "dark.css",
+                  "isFolder": false
+                }
+              ]
+            },
+            {
+              "name": "reset.css",
+              "isFolder": false
+            }
+          ]
+        },
+        {
+          "name": "utils",
+          "isFolder": true,
+          "children": [
+            {
+              "name": "formatDate.js",
+              "isFolder": false
+            },
+            {
+              "name": "validateForm.js",
+              "isFolder": false
+            },
+            {
+              "name": "constants.js",
+              "isFolder": false
+            }
+          ]
+        },
+        {
+          "name": "App.jsx",
+          "isFolder": false
+        },
+        {
+          "name": "index.js",
+          "isFolder": false
+        },
+      ]
+    },    
     {
       name: "package.json",
       isFolder: false,
