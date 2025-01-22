2637206f4f25388353a9efbc83024df47416c4bd
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-22 20:53:26 +0200

    prevent duplicate opened file

--- src/components/RecursiveComponent.tsx
+++ src/components/RecursiveComponent.tsx
@@ -6,13 +6,14 @@ import RenderFileIcon from "./RenderFileIcon";
 import { useDispatch, useSelector } from "react-redux";
 import { setOpenedFiles } from "../app/features/fileTreeSlice";
 import { RootState } from "../app/store";
+import { doesFileObjectExist } from "../utils/functions";
 
 interface IProps {
   fileTree: IFile;
 }
 
 const RecursiveComponent = ({ fileTree }: IProps) => {
-  const { name, isFolder, children } = fileTree;
+  const { id, name, isFolder, children } = fileTree;
   const dispatch = useDispatch();
   const { openedFiles } = useSelector((state: RootState) => state.tree);
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -20,6 +21,12 @@ const RecursiveComponent = ({ fileTree }: IProps) => {
   // ** Handlers
   const toggle = () => {
     setIsOpen((prev) => !prev);
+  };
+
+  const onFileClicked = () => {
+    const exists = doesFileObjectExist(openedFiles, id);
+    if (exists) return;
+    dispatch(setOpenedFiles([...openedFiles, fileTree]));
   };
 
   return (
@@ -37,10 +44,7 @@ const RecursiveComponent = ({ fileTree }: IProps) => {
             <span className="ml-2">{name}</span>
           </div>
         ) : (
-          <div
-            className="mr-2 ml-4 flex items-center"
-            onClick={() => dispatch(setOpenedFiles([...openedFiles, fileTree]))}
-          >
+          <div className="mr-2 ml-4 flex items-center" onClick={onFileClicked}>
             <RenderFileIcon fileName={name} />
             <span className="ml-2">{name}</span>
           </div>
--- src/data/FileTree.tsx
+++ src/data/FileTree.tsx
@@ -1,28 +1,35 @@
 import { IFile } from "../interfaces";
+import { v4 as uuid } from "uuid";
 
 export const fileTree: IFile = {
+  id: uuid(),
   name: "VS Code Clone",
   isFolder: true,
   children: [
     {
+      id: uuid(),
       name: "node_modules",
       isFolder: true,
       children: [
         {
+          id: uuid(),
           name: "react",
           isFolder: true,
           children: [
             {
+              id: uuid(),
               name: "index.js",
               isFolder: false,
             },
           ],
         },
         {
+          id: uuid(),
           name: ".vite",
           isFolder: true,
           children: [
             {
+              id: uuid(),
               name: "index.js",
               isFolder: false,
             },
@@ -31,309 +38,297 @@ export const fileTree: IFile = {
       ],
     },
     {
+      id: uuid(),
       name: "public",
       isFolder: true,
       children: [
         {
+          id: uuid(),
           name: "index.html",
           isFolder: false,
         },
         {
+          id: uuid(),
           name: "favicon.ico",
           isFolder: false,
         },
         {
+          id: uuid(),
           name: "logo192.png",
           isFolder: false,
         },
         {
+          id: uuid(),
           name: "logo512.png",
           isFolder: false,
         },
         {
+          id: uuid(),
           name: "manifest.json",
           isFolder: false,
         },
         {
+          id: uuid(),
           name: "robots.txt",
           isFolder: false,
         },
       ],
     },
     {
-      "name": "src",
-      "isFolder": true,
-      "children": [
+      id: uuid(),
+      name: "src",
+      isFolder: true,
+      children: [
         {
-          "name": "assets",
-          "isFolder": true,
-          "children": []
+          id: uuid(),
+          name: "assets",
+          isFolder: true,
+          children: [],
         },
         {
-          "name": "components",
-          "isFolder": true,
-          "children": [
+          id: uuid(),
+          name: "components",
+          isFolder: true,
+          children: [
             {
-              "name": "Button.jsx",
-              "isFolder": false,
+              id: uuid(),
+              name: "Button.jsx",
+              isFolder: false,
             },
             {
-              "name": "Header.jsx",
-              "isFolder": false,
+              id: uuid(),
+              name: "Header.jsx",
+              isFolder: false,
             },
             {
-              "name": "Sidebar.jsx",
-              "isFolder": false,
+              id: uuid(),
+              name: "Sidebar.jsx",
+              isFolder: false,
             },
             {
-              "name": "Footer.jsx",
-              "isFolder": false,
-            }
-          ]
+              id: uuid(),
+              name: "Footer.jsx",
+              isFolder: false,
+            },
+          ],
         },
         {
-          "name": "context",
-          "isFolder": true,
-          "children": []
+          id: uuid(),
+          name: "context",
+          isFolder: true,
+          children: [],
         },
         {
-          "name": "hooks",
-          "isFolder": true,
-          "children": []
+          id: uuid(),
+          name: "hooks",
+          isFolder: true,
+          children: [],
         },
         {
-          "name": "layouts",
-          "isFolder": true,
-          "children": [
+          id: uuid(),
+          name: "layouts",
+          isFolder: true,
+          children: [
             {
-              "name": "AdminLayout.jsx",
-              "isFolder": false
+              id: uuid(),
+              name: "AdminLayout.jsx",
+              isFolder: false,
             },
             {
-              "name": "InstructorLayout.jsx",
-              "isFolder": false
+              id: uuid(),
+              name: "InstructorLayout.jsx",
+              isFolder: false,
             },
             {
-              "name": "StudentLayout.jsx",
-              "isFolder": false
-            }
-          ]
+              id: uuid(),
+              name: "StudentLayout.jsx",
+              isFolder: false,
+            },
+          ],
         },
         {
-          "name": "pages",
-          "isFolder": true,
-          "children": [
+          id: uuid(),
+          name: "pages",
+          isFolder: true,
+          children: [
             {
-              "name": "admin",
-              "isFolder": true,
-              "children": [
+              id: uuid(),
+              name: "admin",
+              isFolder: true,
+              children: [
                 {
-                  "name": "Dashboard.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Dashboard.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Users.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Users.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Reports.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Reports.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Settings.jsx",
-                  "isFolder": false
-                }
-              ]
+                  id: uuid(),
+                  name: "Settings.jsx",
+                  isFolder: false,
+                },
+              ],
             },
             {
-              "name": "instructor",
-              "isFolder": true,
-              "children": [
+              id: uuid(),
+              name: "instructor",
+              isFolder: true,
+              children: [
                 {
-                  "name": "Dashboard.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Dashboard.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Courses.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Courses.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Students.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Students.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Profile.jsx",
-                  "isFolder": false
-                }
-              ]
+                  id: uuid(),
+                  name: "Profile.jsx",
+                  isFolder: false,
+                },
+              ],
             },
             {
-              "name": "student",
-              "isFolder": true,
-              "children": [
+              id: uuid(),
+              name: "student",
+              isFolder: true,
+              children: [
                 {
-                  "name": "Dashboard.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Dashboard.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Courses.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Courses.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Quizzes.jsx",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "Quizzes.jsx",
+                  isFolder: false,
                 },
                 {
-                  "name": "Notifications.jsx",
-                  "isFolder": false
-                }
-              ]
-            }
-          ]
+                  id: uuid(),
+                  name: "Notifications.jsx",
+                  isFolder: false,
+                },
+              ],
+            },
+          ],
         },
         {
-          "name": "redux",
-          "isFolder": true,
-          "children": [
+          id: uuid(),
+          name: "redux",
+          isFolder: true,
+          children: [
             {
-              "name": "slices",
-              "isFolder": true,
-              "children": [
+              id: uuid(),
+              name: "slices",
+              isFolder: true,
+              children: [
                 {
-                  "name": "authSlice.js",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "authSlice.js",
+                  isFolder: false,
                 },
                 {
-                  "name": "notificationSlice.js",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "notificationSlice.js",
+                  isFolder: false,
                 },
                 {
-                  "name": "quizSlice.js",
-                  "isFolder": false
-                }
-              ]
-            },
-            {
-              "name": "store.js",
-              "isFolder": false
-            }
-          ]
-        },
-        {
-          "name": "routes",
-          "isFolder": true,
-          "children": [
-            {
-              "name": "AdminRoutes.jsx",
-              "isFolder": false
-            },
-            {
-              "name": "InstructorRoutes.jsx",
-              "isFolder": false
-            },
-            {
-              "name": "StudentRoutes.jsx",
-              "isFolder": false
-            },
-            {
-              "name": "ProtectedRoute.jsx",
-              "isFolder": false
-            }
-          ]
-        },
-        {
-          "name": "services",
-          "isFolder": true,
-          "children": [
-            {
-              "name": "authService.js",
-              "isFolder": false
-            },
-            {
-              "name": "quizService.js",
-              "isFolder": false
-            },
-            {
-              "name": "userService.js",
-              "isFolder": false
-            }
-          ]
-        },
-        {
-          "name": "styles",
-          "isFolder": true,
-          "children": [
-            {
-              "name": "variables.css",
-              "isFolder": false
-            },
-            {
-              "name": "themes",
-              "isFolder": true,
-              "children": [
-                {
-                  "name": "light.css",
-                  "isFolder": false
+                  id: uuid(),
+                  name: "quizSlice.js",
+                  isFolder: false,
                 },
-                {
-                  "name": "dark.css",
-                  "isFolder": false
-                }
-              ]
+              ],
             },
             {
-              "name": "reset.css",
-              "isFolder": false
-            }
-          ]
-        },
-        {
-          "name": "utils",
-          "isFolder": true,
-          "children": [
-            {
-              "name": "formatDate.js",
-              "isFolder": false
+              id: uuid(),
+              name: "store.js",
+              isFolder: false,
             },
+          ],
+        },
+        {
+          id: uuid(),
+          name: "routes",
+          isFolder: true,
+          children: [
             {
-              "name": "validateForm.js",
-              "isFolder": false
+              id: uuid(),
+              name: "AdminRoutes.jsx",
+              isFolder: false,
             },
+            { id: uuid(), name: "InstructorRoutes.jsx", isFolder: false },
+            { id: uuid(), name: "StudentRoutes.jsx", isFolder: false },
+            { id: uuid(), name: "ProtectedRoute.jsx", isFolder: false },
+          ],
+        },
+        {
+          id: uuid(),
+          name: "services",
+          isFolder: true,
+          children: [
+            { id: uuid(), name: "authService.js", isFolder: false },
+            { id: uuid(), name: "quizService.js", isFolder: false },
+            { id: uuid(), name: "userService.js", isFolder: false },
+          ],
+        },
+        {
+          id: uuid(),
+          name: "styles",
+          isFolder: true,
+          children: [
+            { id: uuid(), name: "variables.css", isFolder: false },
             {
-              "name": "constants.js",
-              "isFolder": false
-            }
-          ]
+              id: uuid(),
+              name: "themes",
+              isFolder: true,
+              children: [
+                { id: uuid(), name: "light.css", isFolder: false },
+                { id: uuid(), name: "dark.css", isFolder: false },
+              ],
+            },
+            { id: uuid(), name: "reset.css", isFolder: false },
+          ],
         },
         {
-          "name": "App.jsx",
-          "isFolder": false
+          id: uuid(),
+          name: "utils",
+          isFolder: true,
+          children: [
+            { id: uuid(), name: "formatDate.js", isFolder: false },
+            { id: uuid(), name: "validateForm.js", isFolder: false },
+            { id: uuid(), name: "constants.js", isFolder: false },
+          ],
         },
-        {
-          "name": "index.js",
-          "isFolder": false
-        },
-      ]
-    },    
-    {
-      name: "package.json",
-      isFolder: false,
+        { id: uuid(), name: "App.jsx", isFolder: false },
+        { id: uuid(), name: "index.js", isFolder: false },
+      ],
     },
-    {
-      name: "tsconfig.json",
-      isFolder: false,
-    },
-    {
-      name: "vite.config.ts",
-      isFolder: false,
-    },
-    {
-      name: ".gitignore",
-      isFolder: false,
-    },
-    {
-      name: "README.md",
-      isFolder: false,
-    },
+    { id: uuid(), name: "package.json", isFolder: false },
+    { id: uuid(), name: "tsconfig.json", isFolder: false },
+    { id: uuid(), name: "vite.config.ts", isFolder: false },
+    { id: uuid(), name: ".gitignore", isFolder: false },
+    { id: uuid(), name: "README.md", isFolder: false },
   ],
 };
