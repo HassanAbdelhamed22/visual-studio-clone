8947543f224a7a7bc1a6f1f2a5d0d53e8bc9791e
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-19 06:57:36 +0200

    remove tab hover

--- src/components/OpenedFilesBarTab.tsx
+++ src/components/OpenedFilesBarTab.tsx
@@ -19,11 +19,11 @@ const OpenedFilesBarTab = ({ file }: IProps) => {
 
   return (
     <div
-      className="flex items-center justify-center p-2 border-b-[1px] border-[#ffffff1f] hover:bg-[#64646473] duration-300"
+      className="flex items-center justify-center p-2 border-b-[1px] border-[#ffffff1f]"
       onClick={onClick}
     >
       <RenderFileIcon fileName={file.name} />
-      <span className="cursor-pointer duration-300 flex justify-center items-center w-fit mx-1 p-1  rounded-md">
+      <span className="cursor-pointer duration-300 flex justify-center items-center w-fit mx-1 p-1 rounded-md">
         {file.name}
       </span>
       <span className="cursor-pointer hover:bg-[#64646473] duration-300 flex justify-center items-center w-fit mr-2 p-1 rounded-md">
