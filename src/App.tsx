a7b85941b3e34a31abb6aa1e4185806a3d442f6d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-20 19:11:41 +0200

    Add state for managing the search modal and pass the toggleSearchModal function in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,28 @@ import { RootState } from "./app/store";
 import WelcomeTab from "./components/WelcomeTab";
 import IconsSidebar from "./components/IconsSidebar";
 import { useState } from "react";
+import SearchModal from "./components/SearchModal";
 
 function App() {
   const { openedFiles } = useSelector((state: RootState) => state.tree);
   const [showLeftPanel, setShowLeftPanel] = useState(true);
+  const [searchOpen, setSearchOpen] = useState(false);
 
-  // Function to toggle sidebar visibility
   const toggleLeftPanel = () => {
     setShowLeftPanel((prev) => !prev);
+  };
+
+  const toggleSearchModal = () => {
+    setSearchOpen((prev) => !prev);
   };
 
   return (
     <div className="flex h-screen">
       <div className="w-14">
-        <IconsSidebar toggleLeftPanel={toggleLeftPanel}/>
+        <IconsSidebar
+          toggleLeftPanel={toggleLeftPanel}
+          toggleSearchModal={toggleSearchModal}
+        />
       </div>
       <ResizablePanel
         showLeftPanel={showLeftPanel}
@@ -33,6 +41,7 @@ function App() {
         }
         rightPanel={openedFiles.length > 0 ? <Preview /> : <WelcomeTab />}
       />
+      {searchOpen && <SearchModal onClose={toggleSearchModal} />}
     </div>
   );
 }
