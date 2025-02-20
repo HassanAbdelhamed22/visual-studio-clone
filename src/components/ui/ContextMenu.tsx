dae2e5bc92c529ffeecd604fd10ebc0d4353b9b7
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-20 07:09:19 +0200

    use useRef hook in ContextMenu

--- src/components/ui/ContextMenu.tsx
+++ src/components/ui/ContextMenu.tsx
@@ -1,16 +1,21 @@
+import { useRef } from "react";
+
 interface IProps {
   positions: { x: number; y: number };
 }
 
 const ContextMenu = ({ positions }: IProps) => {
+  const menuRef = useRef<HTMLDivElement>(null);
   return (
-    <ul
-      className="bg-white text-black w-fit px-7 py-2 rounded-md"
-      style={{ position: "absolute", left: positions.x, top: positions.y }}
-    >
-      <li>Close</li>
-      <li>Close all</li>
-    </ul>
+    <div ref={menuRef}>
+      <ul
+        className="bg-white text-black w-fit px-7 py-2 rounded-md"
+        style={{ position: "absolute", left: positions.x, top: positions.y }}
+      >
+        <li>Close</li>
+        <li>Close all</li>
+      </ul>
+    </div>
   );
 };
 
