2063100908250a7126345a7dde3238fc20281255
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-20 15:41:02 +0200

    manage the state of showLeftPanel and toggle it when clicking the FileIcon in IconsSidebar

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,30 @@ import { fileTree } from "./data/FileTree";
 import { RootState } from "./app/store";
 import WelcomeTab from "./components/WelcomeTab";
 import IconsSidebar from "./components/IconsSidebar";
+import { useState } from "react";
 
 function App() {
   const { openedFiles } = useSelector((state: RootState) => state.tree);
+  const [showLeftPanel, setShowLeftPanel] = useState(true);
+
+  // Function to toggle sidebar visibility
+  const toggleLeftPanel = () => {
+    setShowLeftPanel((prev) => !prev);
+  };
 
   return (
     <div className="flex h-screen">
       <div className="w-14">
-        <IconsSidebar />
+        <IconsSidebar toggleLeftPanel={toggleLeftPanel}/>
       </div>
       <ResizablePanel
-        showLeftPanel
+        showLeftPanel={showLeftPanel}
         leftPanel={
-          <div className="w-64 p-2">
-            <RecursiveComponent fileTree={fileTree} />
-          </div>
+          showLeftPanel ? (
+            <div className="w-64 p-2">
+              <RecursiveComponent fileTree={fileTree} />
+            </div>
+          ) : null
         }
         rightPanel={openedFiles.length > 0 ? <Preview /> : <WelcomeTab />}
       />
--- src/components/IconsSidebar.tsx
+++ src/components/IconsSidebar.tsx
@@ -2,14 +2,18 @@ import ColorTheme from "./SVG/ColorTheme";
 import FileIcon from "./SVG/File";
 import Search from "./SVG/Search";
 
-interface IProps {}
+interface IProps {
+  toggleLeftPanel: () => void;
+}
 
-const IconsSidebar = ({}: IProps) => {
+const IconsSidebar = ({ toggleLeftPanel }: IProps) => {
   return (
     <div className="bg-[#1e1e1e] h-full shadow-md flex justify-center">
       <div className="h-full py-2 flex flex-col justify-between">
         <div className="p-1 flex flex-col gap-4">
-          <FileIcon height={30} width={30} />
+          <button onClick={toggleLeftPanel}>
+            <FileIcon height={30} width={30} />
+          </button>
           <Search />
         </div>
         <ColorTheme />
